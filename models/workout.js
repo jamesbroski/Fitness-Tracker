6e8b5a1d7e0f97b5c09c76d3be6c14533be7f149
James Broski <james.broski.5@gmail.com>
2021-09-22 19:58:30 -0600

    staring backend models

---
+++ models/workout.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+    },
+  ],
+});
