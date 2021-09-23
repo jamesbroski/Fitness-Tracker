c3c1e410e5486a2b2547d2c8605860160fa55510
James Broski <james.broski.5@gmail.com>
2021-09-22 20:00:40 -0600

    workout model finished

--- models/workout.js
+++ models/workout.js
@@ -13,6 +13,31 @@ const WorkoutSchema = new Schema({
         trim: true,
         required: true,
       },
+      name: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
     },
   ],
 });
+
+const Workout = mongoose.model("workout", WorkoutSchema);
+
+module.exports = Workout;
