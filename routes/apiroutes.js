f28e25455a1254aed4061cf76fa379b7bbef40b2
James Broski <james.broski.5@gmail.com>
2021-09-22 22:48:57 -0600

    nearly functional

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -59,3 +59,70 @@ module.exports = function (app) {
     }
   });
 };
+
+//AGGREGATE and SUM methods
+// module.exports = function (app) {
+//   app.get("/api/workouts", async (req, res) => {
+//     try {
+//       const newestWorkout = await db.Workout.aggregate([
+//         {
+//           $addFields: {
+//             totalDuration: { $sum: "exercises.duration" },
+//             totalWeight: { $sum: "exercises.weight" },
+//             totalSets: { $sum: "exercises.sets" },
+//             totalReps: { $sum: "exercises.reps" },
+//             totalDistance: { $sum: "exercises.distance" },
+//           },
+//         },
+//       ]);
+//       console.log(newestWorkout);
+//       res.json(newestWorkout);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   });
+
+//   app.get("/api/workouts/range", async (req, res) => {
+//     try {
+//       const aggregateData = await db.Workout.aggregate([
+//         {
+//           $addFields: {
+//             totalDuration: { $sum: "exercises.duration" },
+//             totalWeight: { $sum: "exercises.weight" },
+//             totalSets: { $sum: "exercises.sets" },
+//             totalReps: { $sum: "exercises.reps" },
+//             totalDistance: { $sum: "exercises.distance" },
+//           },
+//         },
+//       ]);
+//       res.json(aggregateData);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   });
+
+//   app.post("/api/workouts", async (req, res) => {
+//     try {
+//       const newWorkout = await db.Workout.create(req.body);
+//       res.json(newWorkout);
+//     } catch (error) {
+//       console.log(Erroe);
+//     }
+//   });
+
+//   app.put("/api/workouts/:id", async (req, res) => {
+//     try {
+//       const updatedWorkout = await db.Workout.findOneAndUpdate(
+//         { _id: req.params.id },
+//         {
+//           $push: {
+//             exercises: req.body,
+//           },
+//         }
+//       );
+//       res.json(updatedWorkout);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   });
+// };
